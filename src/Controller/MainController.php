99dd15b2559c15668ba585d75974f5da106e7f84
franclou199 <162106557+franclou199@users.noreply.github.com>
2024-03-26 16:52:01 +0100

    cheh

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Posts;
+use App\Entity\Users;
+use App\Repository\PostsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
-    }
+    // public function index(EntityManagerInterface $em): Response
+    // {
+    //     $author = new Users();
+    //     $author->setEmail("test@test.com")
+    //         ->setFullName("Kerchou")
+    //         ->setUsername("Panda")
+    //         ->setPassword("Panda@29");
+
+    //     $em->persist($author);
+
+    //     $post = new Posts();
+    //     $post->setTitle("un titre") 
+    //         ->setSlug("un-titre")
+    //         ->setContent("un contenu")
+    //         ->setPublishedAt(new \DateTimeImmutable())
+    //         ->setSummary("summary")
+    //         ->setAuthor($author);
+
+    //     $em->persist($post);
+    //     $em->flush();
+
+    //     //dd($post);
+    // }
+
+    public function index(PostsRepository $repository): Response 
+        {
+            // $posts = $repository->findAll();
+            $posts = $repository->findByTitle("un titre");
+
+            dd($posts);
+        }
+
+        
 }
